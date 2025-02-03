dffb4a73035396c4bf38087819eaf0a5caceafa0
Emile Dubois--Darthenucq <edubois-@3A2.42angouleme.fr>
2025-02-03 16:01:32 +0100

    add fill_data

--- srcs/get_data/fill_line_data.c
+++ srcs/get_data/fill_line_data.c
@@ -6,34 +6,75 @@
 /*   By: edubois- <edubois-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:56:25 by edubois-          #+#    #+#             */
-/*   Updated: 2025/02/03 13:11:37 by edubois-         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:59:46 by edubois-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-char *find_cmd_path(char **paths, char *word)
+void	fill_paths(t_data *data)
 {
 	char *tmp_path;
 	char *tmp_path_bis;
+	int		i;
+	int		j;
 
-	tmp_path = ft_strjoin(*paths, "/"); 
-	tmp_path_bis = ft_strjoin(tmp_path, word);
-	free(tmp_path);
-	while (*++paths && access(tmp_path_bis, F_OK) == -1)
+	j = -1;
+	while (data->cmd_list[++j].cmd)
 	{
-	free(tmp_path_bis);
-	tmp_path = ft_strjoin(*paths, "/"); 
-	tmp_path_bis = ft_strjoin(tmp_path, word);
-	free(tmp_path);
+		i = 0;
+		tmp_path = ft_strjoin(data->paths[i], "/"); 
+		tmp_path_bis = ft_strjoinfree(tmp_path, data->cmd_list[j].cmd[0], 1);
+		while (data->paths[++i] && access(tmp_path_bis, F_OK) == -1)
+		{
+			free(tmp_path_bis);
+			tmp_path = ft_strjoin(data->paths[i], "/");
+			tmp_path_bis = ft_strjoinfree(tmp_path, data->cmd_list[j].cmd[0], 1);
+		}
+		if (data->paths[i])
+			data->cmd_list[j].path = tmp_path_bis;
+		else
+		{
+			free(tmp_path_bis);
+			data->cmd_list[j].path = NULL;
+		}
 	}
-	if (*paths)
-		return (tmp_path_bis);
-	free(tmp_path_bis);
-	return (NULL);
 }
 
-static	void	fill_command(t_data *data, char **line)
+int	fill_flags(t_data *data, char **line, int i, int j)
+{
+	int c_flags;
+
+	c_flags = 1;
+	while (line[++j] && line[j][0] == '-')
+	{
+		ft_strapp(&data->cmd_list[i].cmd, line[j]);
+		c_flags++;
+		if (!data->cmd_list[i].cmd)
+			return (0);
+	}
+	return (c_flags);
+}
+
+// void	printcmd(t_data *data)
+// {
+// 	int	 i = 0;
+// 	int j = 0;
+// 	while (data->cmd_list[i].cmd)
+// 	{
+// 		j = 0;
+// 		while (data->cmd_list[i].cmd[j])
+// 		{
+// 			printf("data->cmd_list[%d]->cmd[%d] = %s\n", i, j , data->cmd_list[i].cmd[j]);
+// 			j++;
+// 		}
+// 		printf("%s\n", data->cmd_list[i].path);
+// 		printf("\n\n\n\n\n\n", NULL);
+// 		i++;
+// 	}
+// }
+
+void	fill_command(t_data *data, char **line)
 {
 	int i;
 	int j;
@@ -43,16 +84,11 @@ static	void	fill_command(t_data *data, char **line)
 	data->cmd_list = ft_calloc(ft_arraylen(line) + 1, sizeof(t_cmd));
 	while (line[j] && data->cmd_list)
 	{
-		ft_strapp(&data->cmd_list->cmd, line[j]);
+		ft_strapp(&data->cmd_list[i].cmd, line[j]);
 		if (!data->cmd_list->cmd)
 			return ;
-		if (line [j] && line[j + 1][0] == '-')
-		{	
-			ft_strapp(&data->cmd_list->cmd, line[j + 1]);
-			if (!data->cmd_list->cmd) //a finir
-				return ;
-		}
-		data->cmd_list[i].path = find_cmd_path(&data->cmd_list[i].cmd[0], data->cmd_list->cmd[0]);
+		j += fill_flags(data, line, i, j);
+		i++;
 	}
 }
 
@@ -64,6 +100,8 @@ void    fill_line_data(t_data *data, char *line)
 	if (!full_line)
 		return ;
 	fill_command(data, full_line);
+	fill_paths(data);
+	printcmd(data);
 	add_history(line);
 	free(line);
 }
